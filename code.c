5e804e789f8e73eee57b69042e555b93ba02abb5
LucasclFerreira <lucascosta.ferreira@sou.unifal-mg.edu.br>
2023-05-07 01:58:30 -0300

    algoritmos de forca bruta e guloso adicionados

--- code.c
+++ code.c
@@ -1,17 +1,82 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
 typedef struct {
     unsigned int peso;
     int beneficio;
 } type_item;
 
+// funções e variáveis globais auxiliares
+int maior_peso=0,maior_beneficio=0,tam_solucao_otima=0;
+type_item * solucao_otima;
+
 int encontra_max(int a, int b) {
     if ( a > b) return a;
     return b;
 }
 
+type_item * add(type_item *item,int num_combinacoes, type_item number){
+    //adiciona o item ao vetor de combinações
+    type_item *new_item = (type_item *)malloc((num_combinacoes+1) * sizeof(type_item));
+    for(int i=0;i<num_combinacoes;i++){
+        new_item[i] = item[i];
+    }
+    new_item[num_combinacoes] = number;
+    return new_item;
+}
+
+void permutacao(type_item *item, type_item *combinacoes,int tam_combinacoes, int num_items,int capacidad){
+    //termina a recursão quando o tamanho das combinações for maior que o numero de itens
+    if(tam_combinacoes>num_items){
+        return;
+    }
+    //verifica a soma o peso e o beneficio da combinação atual
+    float beneficio = 0, peso = 0;
+    for(int i=0;i<tam_combinacoes;i++){
+        beneficio+=combinacoes[i].beneficio;
+        peso+=combinacoes[i].peso;
+    }
+    printf("Peso: %f, Beneficio: %f\n",peso,beneficio);
+    //verifica se a combinação atual é melhor que a solução ótima
+    if(peso<=capacidad && beneficio>=maior_beneficio){
+        maior_peso = peso;
+        maior_beneficio = beneficio;
+        tam_solucao_otima = tam_combinacoes;
+        solucao_otima = combinacoes;
+    }
+    //percorre todos os itens
+    for(int i=0;i<num_items;i++){
+        bool contem = false;
+        //verifica se o item atual já está na combinação atual
+        for(int j=0;j<tam_combinacoes;j++){
+            if(item[i].peso == combinacoes[j].peso && item[i].beneficio == combinacoes[j].beneficio){
+                contem = true;
+            }
+        }
+        if(!contem){
+            //chama a função permutação para cada item que não está na combinação atual
+            permutacao(item,add(combinacoes,tam_combinacoes,item[i]),tam_combinacoes+1,num_items,capacidad);
+        }
+        contem = false;
+    }
+}
+
+int compare(const void *a, const void *b) {
+    type_item *itemA = (type_item *)a;
+    type_item *itemB = (type_item *)b;
+    double ratioA = (double)itemA->beneficio / (double)itemA->peso;
+    double ratioB = (double)itemB->beneficio / (double)itemB->peso;
+    if (ratioA > ratioB)
+        return -1;
+    else if (ratioA < ratioB)
+        return 1;
+    else
+        return 0;
+}
+
+// algoritmos de resolução
 type_item *programacao_dinamica(int n_items, int capacidad, type_item *items, int *tam_arr) {
     int i, j;
     
@@ -60,6 +125,61 @@ type_item *programacao_dinamica(int n_items, int capacidad, type_item *items, in
     return items_escolhidos;
 }
 
+void forca_bruta(type_item *item, int num_items, int capacidad) { 
+    /*percorre todos os elementos do vetor de itens e chama a função 
+    permutação para cada um deles gerando todas as combinações possíveis*/
+   for(int i=0;i<num_items;i++){
+        type_item * elemento_unico = (type_item *)malloc(sizeof(type_item));
+        elemento_unico[0] = item[i];
+        permutacao(item,elemento_unico,1,num_items,capacidad); 
+   }
+}
+
+void guloso(type_item *item, int num_items, int capacidad) { 
+    qsort(item, num_items, sizeof(type_item), compare); // ordena os itens
+    type_item *solucao = malloc(num_items * sizeof(type_item));
+    type_item *melhor_solu = malloc(num_items * sizeof(type_item));
+    int armz_peso = 0;
+    int soma_bene = 0;
+    int aux_bene = -1;
+    int b = 0; // guarda a quantidade de itens no vetor solucao
+    int a =0;
+    for(int i = 0; i < num_items; i++){
+    for(int j = 0; j < num_items; j++){
+        if (j != i && armz_peso + item[j].peso <= capacidad){
+            armz_peso += item[j].peso;
+            soma_bene += item[j].beneficio;
+            solucao[b++] = item[j];
+        }
+    } 
+    printf("%d\n" , soma_bene);
+    if(soma_bene > 0){
+        for(int k = 0; k < b; k++){
+            printf("Solução: peso: %d, beneficio: %d \n", solucao[k].peso, solucao[k].beneficio);
+            if(aux_bene <= soma_bene){
+                melhor_solu[k].peso = solucao[k].peso;
+                melhor_solu[k].beneficio = solucao[k].beneficio;
+                aux_bene = soma_bene;
+                a = b;
+            }
+        }  
+    }
+    armz_peso = 0;
+    soma_bene = 0;
+    b = 0;
+  }
+  int max= 0;
+  int max2 = 0;
+   for(int k = 0; k < a; k++){
+            printf("Solução final: peso: %d, beneficio: %d \n", melhor_solu[k].peso, melhor_solu[k].beneficio);
+            max  += melhor_solu[k].beneficio ;
+            max2  += melhor_solu[k].peso ;
+   }
+   printf("\n%d", max);
+   printf("\n%d", max2);
+}
+
+// funções originais do código base
 void imprimir_items(type_item *items, int n) {
     int i;
     for (i = 0; i < n; i++) {
@@ -106,19 +226,35 @@ int main(int argc, char *argv[]) {
     printf("\t\tExecutando o algoritmo...\n");
 
     clock_t inicio_t, fim_t;
-    double total_t_prog_dinam;
+    double total_t_prog_dinam, total_t_forca_bruta, total_t_guloso;
 
-    inicio_t = clock();  // incia cronometragem
+    // programação dinâmica
+    inicio_t = clock();
     type_item *items_escolhidos = programacao_dinamica(n_items, capacidad, items, &tam_arr);  // chama o algoritmo
-    fim_t = clock();  // termina cronometragem
+    fim_t = clock();
     total_t_prog_dinam = (double)(fim_t - inicio_t) / (CLOCKS_PER_SEC / 1000);
+
     for (int i = 0; i < tam_arr; i++) {
         beneficio_max_prog_dinam += items_escolhidos[i].beneficio;
         peso_max_prog_dinam += items_escolhidos[i].peso;
     }
 
+    // força bruta
+    // inicio_t = clock();
+    // forca_bruta(items, n_items, capacidad);
+    // fim_t = clock();
+    // total_t_forca_bruta = (double)(fim_t - inicio_t) / (CLOCKS_PER_SEC/1000);
+
+    // guloso
+    inicio_t = clock();
+    guloso(items, n_items, capacidad);
+    fim_t = clock();
+    total_t_guloso = (double)(fim_t - inicio_t) / (CLOCKS_PER_SEC/1000);
+
     printf("\t\t\tPRONTO!\n\nBENEFICIO E PESO MAXIMO PROGRAMACAO DINAMICA: %d | %d\n", beneficio_max_prog_dinam, peso_max_prog_dinam);
-    printf("Tempo total programacao dinamica: %.0lf milisegundos", total_t_prog_dinam);
+    //printf("Tempo total forca bruta: %.0lf milisegundos\n", total_t_forca_bruta);
+    printf("Tempo total programacao dinamica: %.0lf milisegundos\n", total_t_prog_dinam);
+    printf("Tempo total algoritmo guloso: %.0lf milisegundos\n", total_t_guloso);
 
     free(items_escolhidos);
     free(items);
--- evaluation.py
+++ evaluation.py
@@ -1,33 +1,41 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 # rotulos para o eixo X (na plotagem) dos 10 primeiros conjuntos de testes
 # são strings que contém o tamanho do teste
-rotulos_x_fn = ['10_269', '20_878', '4_20', '4_11', '10_60', '7_50', '23_10000']
+rotulos_x_fn = ['10_269', '20_878', '4_20', '4_11', '15_375', '10_60', '7_50', '23_10000', '5_80', '20_879']
 
 # todos esse dados são referentes aos testes que estão no formato "fx_l-d_kp_xx_xxx" em que x é apenas um exemplo de onde ficaria um número
 # os dados assegui são inventados apenas para testar a plotagem
 # mas poderiam ser trocaos por valores reais para obtermos as imagens
-dados_fn_prog_dinam = pd.Series([2, 6, 1, 0.5, 4, 3, 23], index=rotulos_x_fn, name='prog_dinam')
-dados_fn_forca_bruta = pd.Series([13, 31, 4, 3, 26, 22, 120], index=rotulos_x_fn, name='forca_bruta')
-dados_fn_alg_guloso = pd.Series([4, 12, 2, 1, 8, 7, 56], index=rotulos_x_fn, name='alg_guloso')
+dados_fn_forca_bruta = pd.Series([0, 0, 27, 22, 0, 0, 2732, 0, 136, 0], index=rotulos_x_fn, name='forca_bruta')
+dados_fn_prog_dinam = pd.Series([1, 2, 1, 1, 1, 2, 1, 3, 1, 1], index=rotulos_x_fn, name='programacao_dinamica')
+dados_fn_alg_guloso = pd.Series([38, 118, 6, 3, 70, 28, 6, 109, 11, 70], index=rotulos_x_fn, name='algoritmo_guloso')
 
 # o mesmo pode ser feito para os dados referentes aos teste que estão no formato "knapPI_1_xxxx_xxxx_1" e etc
-dados_knapPI_1 = pd.Series(np.arange(0, 7), index=['100_1000', '200_1000', '500_1000', '1000_1000', '2000_1000', '5000_1000', '10000_1000'], name='fn')
+#dados_knapPI_1_forca_bruta = pd.Series([0, 0, 27, 22, 0, 0, 2732, 0, 136, 0], index=rotulos_x_fn, name='forca_bruta')
+dados_knapPI_1_prog_dinam = pd.Series([1, 1, 16, 48, 152, 1064, 0], index=rotulos_x_fn, name='programacao_dinamica')
+dados_knapPI_1_alg_guloso = pd.Series([385, 1171, 6452, 28339, 113958, 723930, 0], index=rotulos_x_fn, name='algoritmo_guloso')
+
+dados_knapPI_2_prog_dinam = pd.Series([1, 2, 9, 37, 145, 1059, 2969], index=rotulos_x_fn, name='programacao_dinamica')
+dados_knapPI_2_alg_guloso = pd.Series([248, 580, 3714, 14824, 59253, 384967, 1474079], index=rotulos_x_fn, name='algoritmo_guloso')
+
+dados_knapPI_3_prog_dinam = pd.Series([1, 3, 9, 35, 133, 1052, 4152], index=rotulos_x_fn, name='programacao_dinamica')
+dados_knapPI_3_alg_guloso = pd.Series([26, 94, 356, 970, 3487, 20922, 94928], index=rotulos_x_fn, name='algoritmo_guloso')
 
 tabela = pd.concat([dados_fn_prog_dinam, dados_fn_forca_bruta, dados_fn_alg_guloso], axis=1)
-#tabela = pd.DataFrame(tabela)
-
-print(dados_fn_prog_dinam)
-print(dados_fn_forca_bruta)
-print(dados_fn_alg_guloso)
+tabela = pd.DataFrame(tabela)
 
 print(tabela)
 
-#tabela = pd.concat([dados_fn, dados_knapPI_1], axis=1)
-fig, axes = plt.subplots()
-tabela.plot.bar(ax=axes, color=['red', 'green', 'blue'])
-plt.xticks(rotation=20)
+fig = go.Figure(data=[go.Bar(name='Progração Dinâmica', x=tabela.index, y=tabela['programacao_dinamica']),
+                      go.Bar(name='Força Bruta', x=tabela.index, y=tabela['forca_bruta']),
+                      go.Bar(name='Algoritmo Guloso', x=tabela.index, y=tabela['algoritmo_guloso'])])
 
-plt.show()
+# update layout
+fig.update_layout(title='Comparação entre os algoritmos',
+                  xaxis_title='Conjunto de testes',
+                  yaxis_title='Tempo de execução (milisegundos)')
+
+fig.show()
